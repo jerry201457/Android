7d168e25bd696afb92ee71b712006e88388cbea8
jerry201457 <872235631@qq.com>
2018-04-03 14:53:19 +0800

    lwh1

---
+++ picture/src/main/java/com/sinotech/report/main/picture/PictureSelectActivity.java
@@ -0,0 +1,13 @@
+package com.sinotech.report.main.picture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PictureSelectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_select);
+    }
+}
